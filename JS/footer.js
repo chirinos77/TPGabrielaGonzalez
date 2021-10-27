551edde25ff2d83b5f04299e16c9149bc91531df
Gabriela Gonzalez <ggonzalez@psa.com.ar>
2021-10-27 20:45:06 -0300

    first commit

---
+++ JS/footer.js
@@ -0,0 +1,7 @@
+const div_del_footer = document.querySelector('#footer');
+const pie = document.createElement('div');
+pie.className = 'class_footer';
+pie.id = 'id_footer';
+pie.textContent = 'by Gabriela (2021)';
+
+div_del_footer.appendChild(pie);
